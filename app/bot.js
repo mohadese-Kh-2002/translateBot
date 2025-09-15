2412b5f9b896163a8023ee9299490f7fd37beafe
mohadese-Kh-2002 <mohadesekhedri1381@gmail.com>
2025-09-15 20:27:15 +0330

    x

--- app/bot.js
+++ app/bot.js
@@ -1,8 +1,22 @@
 import TelegramBot from "node-telegram-bot-api";
 
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const bot=new TelegramBot(process.env.TOKEN,{polling:true})
-export default bot
+const bot = new TelegramBot(process.env.TOKEN, {
+  polling: {
+    interval: 300,
+    timeout: 30,
+    limit: 100,
+    retryTimeout: 5000,
+    allowedUpdates: [
+      "message",
+      "callback_query",
+      "edited_message",
+      "channel_post",
+      "edited_channel_post",
+    ],
+  },
+});
+export default bot;
